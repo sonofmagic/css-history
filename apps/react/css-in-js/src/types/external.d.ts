f4b8cef9f56272cfae66b53f6feae04cb1afa5e7
ice breaker <1324318532@qq.com>
2025-12-14 02:23:23 +0800

    chore(deps): upgrade

--- apps/react/css-in-js/src/types/external.d.ts
+++ apps/react/css-in-js/src/types/external.d.ts
@@ -1,23 +1,3 @@
-declare module 'styled-components' {
-  const styled: any
-  export default styled
-}
-
-declare module 'styled-jsx/babel'
-
-declare module '@stylexjs/stylex' {
-  export function create<StyleMap extends Record<string, unknown>>(styles: StyleMap): StyleMap
-  export function props(...styles: Array<Record<string, unknown>>): { className: string }
-}
-
-declare module '@vanilla-extract/css' {
-  export function style(styleObject: Record<string, unknown>): string
-}
-
-declare module '@vanilla-extract/vite-plugin' {
-  export function vanillaExtractPlugin(): unknown
-}
-
 declare namespace React {
   interface StyleHTMLAttributes<_T = unknown> {
     jsx?: boolean
